23b153ec46db47c295c64a45aa3d0ef9934596b2
Tanishka <tanishkadeep09@gmail.com>
2024-06-11 17:50:40 +0530

    fixed bug

--- frontend/src/components/FeedbackItem.tsx
+++ frontend/src/components/FeedbackItem.tsx
@@ -8,7 +8,7 @@ interface FeedbackItemProps {
 export const FeedbackItem: React.FC<FeedbackItemProps> = ({ feedback }) => {
   return (
     <div className="border rounded p-4 bg-white shadow-md mt-4">
-      <div className="text-gray-700">
+      <div className="text-gray-700 overflow-auto">
         <b>{feedback.name}</b>: {feedback.feedback}
       </div>
     </div>
