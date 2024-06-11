c911b700a6a0afd7cf6de84ba3de57d084d916b4
Tanishka <tanishkadeep09@gmail.com>
2024-06-11 17:00:55 +0530

    cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import feedbackRoutes from './routes/feedbackRoutes';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import feedbackRoutes from "./routes/feedbackRoutes";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+
 app.use(bodyParser.json());
 
-app.use('/api/feedback', feedbackRoutes);
+app.use("/api/feedback", feedbackRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
